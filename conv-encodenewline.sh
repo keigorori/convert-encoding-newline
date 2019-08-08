f221f376b998ed8862aab313d62b2ae7ab542351
keigorori <www.k544.com@gmail.com>
2019-08-08 19:58:28 +0900

    nkfで指定ディレクトリ以下の文字列と改行コードを変換する処理を実装

---
+++ conv-encodenewline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+directory=$1
+encoding=$2
+newline=$3
+wildcard=$4
+
+IFS_BACKUP=$IFS
+IFS=$'\n'   # 空白文字対策
+for path in `\find $directory -name $wildcard`; do
+    echo $path
+    nkf $encoding $newline --overwrite $path
+done
+IFS=$IFS_BACKUP
+
